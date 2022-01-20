e032236fa58d287159df7526bd0665f5ff859463
sophiaorlando <sophia19orlando@gmail.com>
2022-01-19 21:21:55 -0800

    updated elFin

--- src/components/ElFin/ElFin.jsx
+++ src/components/ElFin/ElFin.jsx
@@ -4,6 +4,33 @@ import './elFin.scss'
 function ElFin() {
   return (
     <div id="elFin" className='elFin'>
+      <div className="container">
+        <div className="item">
+          <h1 className='top1'>Code</h1>
+          <h1 className='btm1'>Test & fix</h1>
+        </div>
+        <div className="item">
+          <h1 className='top1'>Git Repo</h1>
+          <h1 className='btm1'>Test, fix, monitor</h1>
+        </div>
+        <div className="item">
+          <h1 className='top1'>CI/CD</h1>
+          <h1 className='btm1'>Test, fix monitor</h1>
+        </div>
+        <div className="item">
+          <h1 className='approach'>EMPOWERED DEVELOPERS</h1>
+        </div>
+        <div className="item">
+        <h1 className='approach'>AUTOMATED</h1>
+        <h1 className='fixes'>FIXES</h1>
+
+
+        </div>
+        <div className="item">
+        <h1 className='approach'>SECURITY EXPERTISE</h1>
+
+        </div>
+      </div>
       
     </div>
   )
